d7efb2c8cf7df9b4c4c8987b536418d6bdbb9685
trentbarboza <hellajawns@gmail.com>
2022-02-11 09:37:28 -0800

    updated why again

--- src/Pages/WhyPage.js
+++ src/Pages/WhyPage.js
@@ -12,10 +12,11 @@ export default function WhyPage() {
         <h1 className='homeText mt-4 text-center'>Why Mark Gonzales?</h1>
       </Row>
         <Row>
-        <p className='descText text-left mt-3'>I chose Mark Gonzales because skateboarding has and will always be a huge part of my life. He is known as the creator of "street skating" and since I stepped on a skateboard when I was a child, that's majority of what I was doing. When I turned 13 I began to actually learn how to do tricks. Throughout my whole teenage years, before I had a car, my skateboard was my main form of transportation. It's taken me all over the world, from traveling to different states and skating at skateparks or just riding down the street to grab some food at the store. I could always rely on my legs and skateboard to get me there. I have met some of best and most intresting people in life through skateboading.</p>
-        <p className='descText text-left'>
-          Not only does Gonz the skateboarder influence me but his artistic side does as well. Since a child, just like most kids, I loved to draw and create things. That has stuck with me throughout my life with photography. He influences me to just not be stuck in a box to one thing. That, I myself and we all are capable of doing multiple great things in our time.
+        <p className='descText mt-3'>I chose Mark Gonzales because skateboarding has and will always be a huge part of my life. He is known as the creator of "street skating" and since I stepped on a skateboard when I was a child, that's majority of what I was doing. When I turned 13 I began to actually learn how to do tricks. Throughout my whole teenage years, before I had a car, my skateboard was my main form of transportation. It's taken me all over the world, from traveling to different states and skating at skateparks to just riding down the street and grabbing some food at the store. I could always rely on my legs and skateboard to get me there.</p>
+        <p className='descText'>
+          Not only does Gonz the skateboarder influence me but his artistic side does as well. Since a child, just like most kids, I loved to draw and create things. That has stuck with me throughout my life with photography. Mark isn't the best or most talented artist in the world, some would think he's not nearly even close but that doesn't stop him from creating with top brands such as Adidas & Supreme, it's clear people still love his work. That truly influences me to keep going and to always do what I enjoy. He influences me to just not be stuck in a box to one thing also. That, I myself and we all are capable of doing multiple great things in our time here.
         </p>
+        <p className='descText'><strong>Thank You Gonz, -Trent</strong></p>
         </Row>
       </Col>
       <Col>
